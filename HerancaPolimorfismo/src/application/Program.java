306ac7d18548ed8d58c879113cc929673e771e7b
prp-git <paulrp@gmail.com>
2021-06-19 14:31:06 -0300

    Atualização de repositório

---
+++ HerancaPolimorfismo/src/application/Program.java
@@ -0,0 +1,60 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.ImportedProduct;
+import entities.Product;
+import entities.UsedProduct;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		
+		System.out.print("Enter the number of products: ");
+		int N = sc.nextInt();
+		
+		List<Product> list = new ArrayList<>();
+				
+		for (int i=1; i<=N; i++) {
+			System.out.print("Product #" + i + " data:");
+			System.out.print("Common, used or imported (c/u/i)?");
+			char type = sc.next().charAt(0);
+			System.out.print("Name:");
+			String name = sc.next();
+			System.out.print("Price:");
+			Double price = sc.nextDouble();
+			if (type == 'i') {
+				System.out.print("Customs fee:");
+				Double customsFee = sc.nextDouble();
+				list.add(new ImportedProduct(name, price, customsFee));
+			}
+			else if (type == 'u') {
+				System.out.println("Manufacture date (DD/MM/YYYY):");
+				Date manufactureDate = sdf.parse(sc.next());
+				list.add(new UsedProduct(name, price, manufactureDate));
+			
+			}
+			else
+				list.add(new Product(name, price));
+		}
+		System.out.println();
+		System.out.println("PRICE TAGS:");
+		for (Product prod : list) {
+			System.out.println(prod.priceTag());
+		}
+		sc.close();
+	}
+}	
+	
+		
